be4cd2433a97a2f3d2cab029c4f2f5a872251459
Justin <justin@appint.cn>
2023-11-22 11:53:32 +0800

    add Azure api sport

--- lib/src/instance/chat/chat.dart
+++ lib/src/instance/chat/chat.dart
@@ -204,6 +204,7 @@ interface class OpenAIChat implements OpenAIChatBase {
         if (responseFormat != null) "response_format": responseFormat,
       },
       onSuccess: (Map<String, dynamic> response) {
+        print(response);
         return OpenAIStreamChatCompletionModel.fromMap(response);
       },
       client: client,
